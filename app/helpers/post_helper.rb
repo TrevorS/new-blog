427c75cc4200eec1d4094bfc8be9c144081c730d
Trevor Strieber <trevor@strieber.org>
2013-10-22 17:14:04 -0400

    Working on colors and styling.

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,7 +1,7 @@
 module PostHelper
-  def get_post_link(post)
-    link_to post.title, post
-  end
+  def get_post_link(post, color)
+    link_to post.title, post, class: color
+ end
 
   def get_post_creation(post)
     post.created_at.strftime('%B %e, %Y')
